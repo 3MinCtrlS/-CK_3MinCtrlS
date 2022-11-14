04ba78201a77f56f29e72491efb0fd90f0b88f59
JuNijen <wjstldms0823@naver.com>
2022-11-15 01:16:01 +0900

    [FEAT] 퍼즐 게임 : default settings
    [프로그래밍] 퍼즐 게임 - Test Map & Player Setting#46
    [프로그래밍] 퍼즐 게임 - add character movement#47
    [프로그래밍] 퍼즐 게임 - add camera moving : following player#48
    
    
    Signed-off-by: JuNijen <wjstldms0823@naver.com>

---
+++ PresssTheCommand/Assets/Scripts/Camera.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// 작성자 : 전시은
+// 작성일 : 2022.11.14
+// 사용 방법 : MovePosition()을 이용하여 Player를 추적하게끔 동작.
+// 제작 사유 : Camera 객체 컨트롤.
+public class Camera : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    private Vector3 offset;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Initalize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePosition();
+    }
+
+
+    // 초기 환경 셋팅
+    private void Initalize()
+    {
+        Debug.Log("Player :: Initalize - Done");
+    }
+
+
+    // Camera follows the player with specified offset position
+    // Player가 무빙 할 때에만 message를 토스하는 등 동작을 제어 할 수 있는지 검토 필요.
+    private void MovePosition()
+    {
+        transform.position = new Vector3(player.position.x + offset.x, player.position.y + offset.y, -10);
+    }
+}
